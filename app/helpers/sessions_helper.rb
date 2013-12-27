71284b37861f6f0f91b7d81958c7362ff3a0a80d
shahriark <shahriar@mit.edu>
2013-12-27 15:47:26 -0500

    Finish user edit, updateindex and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,11 @@ def sign_in(user)
     self.current_user = user
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+
   def current_user=(user)
     @current_user = user
   end
@@ -24,6 +29,15 @@ def signed_in?
     current_user.update_attribute(:remember_token,          User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+ def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -106,6 +106,20 @@ describe "when email address is already taken" do
   end
 
   end
+it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
 
 end
